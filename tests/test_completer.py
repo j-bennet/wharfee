20705b82e5622fa65dfa78edced7896336c9ff40
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-04-23 09:09:08 -0700

    Added more tests for command name and option completions.

--- tests/test_completer.py
+++ tests/test_completer.py
@@ -18,8 +18,7 @@ def complete_event():
 
 def test_empty_string_completion(completer, complete_event):
     """
-    Test that in the beginning of the line, all available commands are
-    suggested.
+    In the beginning of the line, all available commands are suggested.
     """
     text = ''
     position = 0
@@ -29,3 +28,60 @@ def test_empty_string_completion(completer, complete_event):
     assert result == set(map(Completion, completer.commands))
 
 
+def test_matching_command_completion(completer, complete_event):
+    """
+    Beginning of the command should suggest the command.
+    """
+    _test_command_completion(completer, complete_event, 'h', 'help')
+    _test_command_completion(completer, complete_event, 'he', 'help')
+    _test_command_completion(completer, complete_event, 'hel', 'help')
+    _test_command_completion(completer, complete_event, 'help', 'help')
+
+    _test_command_completion(completer, complete_event, 'i', 'images')
+    _test_command_completion(completer, complete_event, 'im', 'images')
+    _test_command_completion(completer, complete_event, 'ima', 'images')
+    _test_command_completion(completer, complete_event, 'imag', 'images')
+
+
+def test_options_completion(completer, complete_event):
+    """
+    After command name, suggest command options.
+    """
+    _test_options_completion(
+        completer, complete_event, 'ps ', ['--help', '--all', '--quiet', '--latest', '--size'], 0)
+
+    _test_options_completion(
+        completer, complete_event, 'ps --', ['--help', '--all', '--quiet', '--latest', '--size'], -2)
+
+    _test_options_completion(
+        completer, complete_event, 'ps --h', ['--help'], -3)
+
+
+def _test_command_completion(completer, complete_event, command, expected):
+    """
+    Helper method to test command suggestions.
+    :param command: string: text that user started typing
+    :param expected: string: expected completion
+    """
+    position = len(command)
+    result = set(completer.get_completions(
+        Document(text=command, cursor_position=position),
+        complete_event))
+    assert result == {Completion(expected, -len(command))}
+
+
+def _test_options_completion(completer, complete_event, command, expected, expected_pos):
+    """
+    Helper method to test command options suggestions.
+    :param command: string: text that user started typing
+    :param expected: list: expected completions
+    """
+    position = len(command)
+
+    result = set(completer.get_completions(
+        Document(text=command, cursor_position=position),
+        complete_event))
+
+    expected = set(map(lambda t: Completion(t, expected_pos), expected))
+
+    assert result == expected
