f17daf8fc4745c540f8f5b098cb2e3d5a654b206
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-04-29 21:44:21 -0700

    Fiddling with formatting.

--- dockercli/formatter.py
+++ dockercli/formatter.py
@@ -25,6 +25,60 @@ def format_data(data):
     return data
 
 
+def format_struct(data, spaces=1, indent=0, lines=None):
+
+    if lines is None:
+        lines = []
+
+    if isinstance(data, dict):
+        data = [(k, data[k]) for k in sorted(data.keys())]
+
+    def add_item_to_line(current_item, current_line, is_last_item):
+        """ Helper to add item to end of line """
+        current_indent = ' ' * indent
+        current_line = current_indent + \
+                       current_line + \
+                       '{0}'.format(current_item)
+        if is_last_item:
+            lines.append(current_line)
+        else:
+            current_line += ': '
+        return current_line
+
+    for row in data:
+        line = ''
+        l = len(row)
+        for i in range(l):
+            if isinstance(row[i], dict):
+                lines.append(line)
+                lines = format_struct(row[i], spaces, indent + 1, lines)
+                indent -= 1
+            elif isinstance(row[i], list):
+                if is_plain_list(row[i]):
+                    item = ', '.join(map(lambda x: '{0}'.format(x), row[i]))
+                    line = add_item_to_line(item, line, i == (l - 1))
+                else:
+                    lines.append(line)
+                    lines = format_struct(row[i], spaces, indent + 1, lines)
+                    indent -= 1
+            else:
+                line = add_item_to_line(row[i], line, i == (l - 1))
+
+    return lines
+
+
+def is_plain_list(lst):
+    """
+    Check if all items in list are strings or numbers
+    :param lst:
+    :return: boolean
+    """
+    for item in lst:
+        if not isinstance(item, basestring) and \
+                not isinstance(item, (int, long, float, complex)):
+            return False
+    return True
+
 def flatten_rows(rows):
     """
     Transform all list or dict values in a dict into comma-separated strings.
