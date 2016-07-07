c125f0e05aba5578d53d970372fa3a97c08704b8
Irina Truong <i.chernyavska@gmail.com>
2016-07-07 14:56:32 -0700

    Tests for image-related commands.

--- tests/features/image_commands.feature
+++ tests/features/image_commands.feature
@@ -4,7 +4,7 @@ Feature: call image-related commands
      Given we have wharfee installed
       when we run wharfee
       and we wait for prompt
-      and we build image from Dockerfile
+      and we build test-image from Dockerfile
       then we see image built
 
   Scenario: pull hello-world image
@@ -13,3 +13,47 @@ Feature: call image-related commands
       and we wait for prompt
       and we pull hello-world image
       then we see hello-world pulled
+
+  Scenario: list images
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we list images
+      then we see image hello-world listed
+
+  Scenario: inspect image
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we inspect hello-world
+      then we see "hello-world:latest" output
+
+  Scenario: log in as user wharfee
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      and we log in as wharfee with docker.me
+      then we see login success
+
+  Scenario: tag an image
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we tag test-image into wharfee/test-image:version1.0
+      then we see test-image tagged into wharfee/test-image:version1.0
+
+  Scenario: remove an image
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we remove image wharfee/test-image:version1.0
+      then we see image wharfee/test-image:version1.0 removed
+      when we remove image test-image
+      then we see image test-image removed
+
+  Scenario: search images
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we search for nginx
+      then we see "Official build of Nginx" output
--- tests/features/steps/image_commands.py
+++ tests/features/steps/image_commands.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 from behave import when, then
 
 
-@when('we build image from Dockerfile')
-def step_build_dockerfile(context):
+@when('we build {image_name} from Dockerfile')
+def step_build_dockerfile(context, image_name):
     """
     Send "build -t test-image ./tests/features/fixture_data/".
     """
-    context.cli.sendline('build -t test-image ' + context.data_dir)
+    context.cli.sendline('build -t {} {}'.format(image_name, context.data_dir))
 
 
 @then('we see image built')
@@ -37,3 +37,91 @@ def step_see_image_pulled(context, image_name):
         'Downloaded newer image for ' + image_name,
         'Image is up to date for ' + image_name
     ])
+
+
+@when('we log in as {user} with {password}')
+def step_log_in(context, user, password):
+    """
+    Send "login" command.
+    """
+    context.cli.sendline('login -u {} -p {}'.format(user, password))
+
+
+@then('we see login success')
+def step_see_log_in_success(context):
+    """
+    Expect to see login succeeded.
+    """
+    context.cli.expect_exact('Login Succeeded')
+
+
+@when('we tag {image_name} into {repo_name}')
+def step_tag_image(context, image_name, repo_name):
+    """
+    Send "tag" command.
+    """
+    context.cli.sendline('tag {} {}'.format(image_name, repo_name))
+
+
+@then('we see {image_name} tagged into {repo_name}')
+def step_see_image_tagged(context, image_name, repo_name):
+    """
+    Expect to see image tagged.
+    """
+    context.cli.expect_exact('Tagged {} into {}'.format(image_name, repo_name))
+
+
+@when('we remove image {image_name}')
+def step_remove_image(context, image_name):
+    """
+    Send "rmi" command.
+    """
+    context.cli.sendline('rmi {}'.format(image_name))
+
+
+@then('we see image {image_name} removed')
+def step_see_image_removed(context, image_name):
+    """
+    Expect to see image removed.
+    """
+    context.cli.expect_exact(image_name)
+
+
+@when('we list images')
+def step_list_images(context):
+    """
+    Send "images" command.
+    """
+    context.cli.sendline('images')
+
+
+@then('we see image {image_name} listed')
+def step_see_image_listed(context, image_name):
+    """
+    Expect to see image listed.
+    """
+    context.cli.expect_exact('hello-world')
+
+
+@when('we inspect {name}')
+def step_inspect(context, name):
+    """
+    Send "inspect" command.
+    """
+    context.cli.sendline('inspect {}'.format(name))
+
+
+@when('we search for {name}')
+def step_search(context, name):
+    """
+    Send "search" command.
+    """
+    context.cli.sendline('search {}'.format(name))
+
+
+@then('we see {text} output')
+def step_see_output(context, text):
+    """
+    Expect to see ouptut.
+    """
+    context.cli.expect_exact([text, text.strip('"')])
