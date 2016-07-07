7dc94be55a0d46cfbff31f242711cd421228a117
Irina Truong <i.chernyavska@gmail.com>
2016-07-06 22:09:34 -0700

    Integration test for pull.

--- tests/features/image_commands.feature
+++ tests/features/image_commands.feature
@@ -6,3 +6,11 @@ Feature: call image-related commands
       and we wait for prompt
       and we build image from Dockerfile
       then we see image built
+
+  @wip
+  Scenario: pull hello-world image
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      and we pull hello-world image
+      then we see hello-world pulled
--- tests/features/steps/image_commands.py
+++ tests/features/steps/image_commands.py
@@ -17,4 +17,23 @@ def step_see_image_built(context):
     """
     Expect to see image built.
     """
-    context.cli.expect('Successfully built')
+    context.cli.expect_exact('Successfully built')
+
+
+@when('we pull {image_name} image')
+def step_pull_image(context, image_name):
+    """
+    Send "pull {image_name}".
+    """
+    context.cli.sendline('pull ' + image_name)
+
+
+@then('we see {image_name} pulled')
+def step_see_image_pulled(context, image_name):
+    """
+    Expect to see image pulled.
+    """
+    context.cli.expect_exact([
+        'Downloaded newer image for ' + image_name,
+        'Image is up to date for ' + image_name
+    ])
