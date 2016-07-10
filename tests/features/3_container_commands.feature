d550cad4a16385a5f617bd4d904b3391cd703c52
Irina Truong <i.chernyavska@gmail.com>
2016-07-10 16:19:51 -0700

    Added integration tests for run -d, exec, stop.

--- tests/features/3_container_commands.feature
+++ tests/features/3_container_commands.feature
@@ -7,10 +7,23 @@ Feature: call container-related commands
       when we run container hello with image hello-world
       then we see "Hello from Docker!" printed out
 
-  @wip
   Scenario: remove container
      Given we have wharfee installed
       when we run wharfee
       then we see wharfee prompt
       when we remove container hello
       then we see hello at line end
+
+  Scenario: run container, exec, stop, rm
+     Given we have wharfee installed
+      when we run wharfee
+      and we wait for prompt
+      when we run container foo with image busybox and command /bin/sh and options -d -i -t
+      and we wait for prompt
+      then we see "Interactive terminal is closed" printed out
+      when we execute ls -l / in container foo
+      then we see total 36 at line end
+      when we stop container foo
+      then we see foo at line end
+      when we remove container foo
+      then we see foo at line end
--- tests/features/steps/3_container_commands.py
+++ tests/features/steps/3_container_commands.py
@@ -5,12 +5,42 @@ import wrappers
 from behave import when, then
 
 
+@when('we run container {container_name} with image {image_name} and command {command} and options {options}')
+def step_run_container_with_command(context, container_name, image_name, command, options):
+    """
+    Send "run" with command.
+    """
+    context.cli.sendline('run {3} --name {0} {1} {2}'.format(
+        container_name,
+        image_name,
+        command,
+        options))
+
+
 @when('we run container {container_name} with image {image_name}')
 def step_run_container(context, container_name, image_name):
     """
+    Send "run" with command.
+    """
+    context.cli.sendline('run --name {0} {1}'.format(
+        container_name,
+        image_name))
+
+
+@when('we execute {command_name} in container {container_name}')
+def step_exec_command(context, command_name, container_name):
+    """
     Send "run" command.
     """
-    context.cli.sendline('run --name {0} {1}'.format(container_name, image_name))
+    context.cli.sendline('exec {0} {1}'.format(container_name, command_name))
+
+
+@when('we stop container {container_name}')
+def step_stop_container(context, container_name):
+    """
+    Send "stop" command.
+    """
+    context.cli.sendline('stop {0}'.format(container_name))
 
 
 @when('we remove container {name}')
