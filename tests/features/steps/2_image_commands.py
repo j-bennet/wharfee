55463ad1888c8f2fb4ee3dcc2ce89b0d8e5ca4b0
Irina Truong <i.chernyavska@gmail.com>
2016-12-08 19:13:09 -0800

    Fixed indent.

--- tests/features/steps/2_image_commands.py
+++ tests/features/steps/2_image_commands.py
@@ -36,9 +36,8 @@ def step_see_image_pulled(context, image_name):
     """
     wrappers.expect_exact(context, [
         'Downloaded newer image for ' + image_name,
-        'Image is up to date for ' + image_name
-    ],
-    timeout=180)
+        'Image is up to date for ' + image_name],
+        timeout=180)
 
 
 @when('we log in as {user}, {email} with {password}')
